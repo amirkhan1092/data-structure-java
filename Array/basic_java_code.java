183bd5714d2947177aa0a71484fed39d3cea492f
amirkhan1092 <amirkhan1092@gmail.com>
2022-11-25 01:03:03 +0530

    Java basic

---
+++ Array/basic_java_code.java
@@ -0,0 +1,12 @@
+import java.net.SocketPermission;
+
+// addition of two number 
+
+class basic_java_code{
+
+    public static void main(String arg[])
+    {
+        System.out.println("Hello World");
+    }
+
+}
