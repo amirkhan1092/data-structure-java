6abe613ded1f2eaba9586107ea3774902da489b7
amirkhan1092 <amirkhan1092@gmail.com>
2022-11-25 01:03:27 +0530

    Addition sample

---
+++ Basic/Addition.java
@@ -0,0 +1,15 @@
+// package Basic;
+
+public class Addition {
+    public static void main(String argv[])
+    {
+        System.out.println("Hello World"+34);
+        int out = sum(10, 30);
+        System.err.println(out); 
+    }
+
+    public static int sum(int a, int b)
+    {
+        return a + b;
+    }
+}
