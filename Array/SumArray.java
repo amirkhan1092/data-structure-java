dc664bf1bdc01771257d2e24ab54422ffb8195e8
amirkhan1092 <amirkhan1092@gmail.com>
2022-11-27 02:45:41 +0530

    updating...

--- Array/SumArray.java
+++ Array/SumArray.java
@@ -1,12 +1,18 @@
 public class SumArray {
     public static void main(String[] args) {
         int[] array = { 87, 68, 94, 100, 83, 78, 85, 91, 76, 87 };
-        int total = 0;
+        // int total = 0;
 
         // add each element's value to total
-        for (int counter = 0; counter < array.length; counter++)
-            total += array[counter];
+        // for (int counter = 0; counter < array.length; counter++)
+        //     total += array[counter];
 
-        System.out.printf("Total of array elements: %d%n", total);
+        // System.out.printf("Total of array elements: %d%n", total);
+
+        // For-each Loop for Java Array
+        int totalForEach = 0;
+        for (int number : array)
+            totalForEach += number;
+        System.out.println("Total of array elements using for-each loop: " + totalForEach);
     }
 }
