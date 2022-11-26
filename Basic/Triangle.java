8dda3df2ce9be40cbce42cc6bf7c95e2e4c20f85
amirkhan1092 <amirkhan1092@gmail.com>
2022-11-26 12:26:03 +0530

    with scanner close

--- Basic/Triangle.java
+++ Basic/Triangle.java
@@ -2,17 +2,21 @@ import java.util.Scanner;
 
 public class Triangle {
     public static void main(String [] argv)
-    {
-        Scanner sc = new Scanner(System.in);
-        // int rows;
-        int rows = sc.nextInt();
-        for(int i=1; i<=rows; i++)
-        {
-            for(int j=0; j<i; j++)
+    {  
+        try (Scanner sc = new Scanner(System.in)) {
+            // int rows;
+            int rows = sc.nextInt();
+            for(int i=1; i<=rows; i++)
             {
-                System.out.print("*");
+                for(int j=0; j<i; j++)
+                {
+                    System.out.print("*");
+                }
+                System.out.println();
             }
-            System.out.print("\n");
+        }
+        catch (Exception e) {
+            System.out.println("Problem with Input and Output");
         }
 
 
